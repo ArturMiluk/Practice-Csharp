04e20f07051b1511ddad98e01b3d63ef9a006e90
Artur Miluk <milukartur@gmail.com>
2026-03-20 13:10:26 +0300

    first commit

---
+++ Day_4/Task_7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            double celsiusValue = 25.0;
+            double fromCelsius = CelsiusToFahrenheit(celsiusValue);
+            Console.WriteLine($"Цельсий {celsiusValue}°C = {fromCelsius:F1}°F");
+
+            double kelvinValue = 298.15;
+            double fromKelvin = KelvinToFahrenheit(kelvinValue);
+            Console.WriteLine($"Кельвин {kelvinValue}K = {fromKelvin:F1}°F");
+
+            Console.ReadKey();
+        }
+
+        static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        static double KelvinToFahrenheit(double kelvin)
+        {
+            var celsius = kelvin - 273.15;
+
+            return CelsiusToFahrenheit(celsius);
+        }
+    }
+
+}
---
+++ Day_4/Task_8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            double d1 = 3.5;
+            double d2 = 2.5;
+            CompareNumbers(in d1, in d2, out string res1);
+            Console.WriteLine($"Результат сравнения {d1} и {d2}: {res1}");
+
+            int i1 = 4;
+            int i2 = 4;
+            CompareNumbers(in i1, in i2, out string res2);
+            Console.WriteLine($"Результат сравнения {i1} и {i2}: {res2}");
+
+            Console.ReadKey();
+        }
+
+        static void CompareNumbers(in double a, in double b, out string result)
+        {
+            if (a > b)
+            {
+                result = "больше";
+            }
+            else if (a < b)
+            {
+                result = "меньше";
+            }
+            else
+            {
+                result = "равно";
+            }
+        }
+
+        static void CompareNumbers(in int a, in int b, out string result)
+        {
+            if (a > b)
+            {
+                result = "больше";
+            }
+            else if (a < b)
+            {
+                result = "меньше";
+            }
+            else
+            {
+                result = "равно";
+            }
+        }
+    }
+}
