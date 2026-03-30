9b8885e50c36f4a60d3b43bd932fc0a4a7d43202
Artur Miluk <milukartur@gmail.com>
2026-03-30 10:40:20 +0300

    first commit

---
+++ Day_12/Task_1/StudentRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudentDiary
+{
+    public class StudentRecord
+    {
+        public string Subject {get; set;}
+
+        public DateTime Date {get; set;}
+
+        public int Grade {get; set;}
+    }
+}
---
+++ Day_12/Task_1/Window1.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using Task_1;
+
+namespace StudentDiary
+{
+    public partial class Window1 : Window
+    {
+        private ObservableCollection<StudentRecord> records;
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            records = new ObservableCollection<StudentRecord>
+            {
+                new StudentRecord
+                {
+                    Subject = "КПиЯП",
+                    Date = DateTime.Today,
+                    Grade = 7
+                },
+                new StudentRecord
+                {
+                    Subject = "ТПО",
+                    Date = DateTime.Today.AddDays(+1),
+                    Grade = 6
+                }
+            };
+
+            scheduleGrid.ItemsSource = records;
+        }
+
+        private void OnAddGradeClick(object sender, RoutedEventArgs e)
+        {
+            var newRecord = new StudentRecord
+            {
+                Subject = "Новый предмет",
+                Date = DateTime.Today,
+                Grade = 5
+            };
+
+            records.Add(newRecord);
+        }
+
+        private void OnFilterClick(object sender, RoutedEventArgs e)
+        {
+            if (dateFilterPicker.SelectedDate == null)
+            {
+                scheduleGrid.ItemsSource = records;
+                return;
+            }
+
+            DateTime selectedDate = dateFilterPicker.SelectedDate.Value;
+
+            var filtered = records
+                .Where(r => r.Date.Date == selectedDate.Date)
+                .ToList();
+
+            scheduleGrid.ItemsSource = filtered;
+        }
+    }
+}
